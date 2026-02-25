f8af231d9b0ca4f4007e45ccdadb368896abb258
Abdelrahmansherifkamel <89721625+Abdelrahmansherifkamel@users.noreply.github.com>
2026-02-25 23:05:51 +0200

    Add files via upload

---
+++ jelly-tester/src/main/java/oddOReven.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class oddOReven {
+    public String check(int number) {
+        if (number % 2 == 0) {
+            return "Even";
+        } else {
+            return "Odd";
+        }
+    }
+}
